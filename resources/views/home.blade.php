a678f7a5b962fdfb2d8ec607f213dbadf66f7cb8
justindu1017 <34846425+justindu1017@users.noreply.github.com>
2021-02-04 20:20:42 +0800

    init commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-4 p-5" style="  display: flex;justify-content: center;align-items: center;">
+                <img style="width:75pt;height: 75pt;" src="https://instagram.ftpe7-3.fna.fbcdn.net/v/t51.2885-19/s320x320/97566921_2973768799380412_5562195854791540736_n.jpg?_nc_ht=instagram.ftpe7-3.fna.fbcdn.net&_nc_ohc=cSMPrDNj5aMAX9-185w&tp=1&oh=ef547f2f67fa093a33a5125c30bda6c3&oe=6044E71F" class="rounded-circle">
+        </div>
+        
+        <div class="col-8">
+            <div><h1>freecodecamp</h1></div>
+            <div style="display:flex">
+                <div class="pr-3"><strong>358 貼文</strong></div>
+                <div class="pr-3"><strong>6.5萬 位追蹤者</strong></div>
+                <div class="pr-3"><strong>295 追蹤中</strong></div>
+            </div>
+            <div><strong>freeCodeCamp.org</strong></div>
+            <div>We're a global community of millions of people learning to code together. We're an open source, donor-supported, 501(c)(3) nonprofit.</div>
+            <div><a href="www.freecodecamp.org">www.freecodecamp.org</a></div>
+        </div>
+    </div>
+
+
+    <div class="row pt-5">
+        <div class="col-4">
+            <img class="w-100" src="https://instagram.ftpe7-2.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/c2.0.823.823a/s640x640/137282729_891157201697073_3007940832466214946_n.jpg?_nc_ht=instagram.ftpe7-2.fna.fbcdn.net&_nc_cat=109&_nc_ohc=m9GMJh5C1-sAX_fu3w5&tp=1&oh=c145c1adaa46ed2733e1cbee8b71bef5&oe=6045868E">
+        </div>
+        <div class="col-4">    
+            <img class="w-100" src="https://instagram.ftpe7-2.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/c2.0.823.823a/s640x640/137282729_891157201697073_3007940832466214946_n.jpg?_nc_ht=instagram.ftpe7-2.fna.fbcdn.net&_nc_cat=109&_nc_ohc=m9GMJh5C1-sAX_fu3w5&tp=1&oh=c145c1adaa46ed2733e1cbee8b71bef5&oe=6045868E">
+        </div>
+        <div class="col-4"></div>
+    </div>
+</div>
+@endsection
